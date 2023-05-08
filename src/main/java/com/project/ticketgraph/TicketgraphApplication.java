32d353e0aad20f4f7414740e25ad1b7af79f8292
Hussain <2135301@cognizant.com>
2023-05-07 23:57:58 -0700

    initial commit

---
+++ src/main/java/com/project/ticketgraph/TicketgraphApplication.java
@@ -0,0 +1,13 @@
+package com.project.ticketgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketgraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ticketgraph/TicketgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ticketgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketgraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
